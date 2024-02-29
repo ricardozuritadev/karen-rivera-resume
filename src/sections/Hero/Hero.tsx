6ba9b040024bcda562a836b5ffdeb66579f191ec
Ricardo Zurita <ricardo.zurita.dev@gmail.com>
2024-02-29 13:55:08 +0100

    Create Hero section

---
+++ src/sections/Hero/Hero.tsx
@@ -0,0 +1,11 @@
+export default function Hero() {
+  return (
+    <section className="hero-section">
+      <p className="hero-section__text">!Hi! I'm karen</p>
+      <h1>
+        <span>product</span>
+        <span className="hero-section__text--yellow">designer</span>
+      </h1>
+    </section>
+  );
+}
