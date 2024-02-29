ffb4ebee4e0c9af411e5caa9268946deabf78c61
Ricardo Zurita <ricardo.zurita.dev@gmail.com>
2024-02-29 18:19:19 +0100

    Modify classNames

--- src/sections/Hero/Hero.tsx
+++ src/sections/Hero/Hero.tsx
@@ -1,10 +1,10 @@
 export default function Hero() {
   return (
-    <section className="hero-section">
-      <p className="hero-section__text">!Hi! I'm karen</p>
+    <section className="hero">
+      <p className="hero__text">Hi! I'm karen</p>
       <h1>
         <span>product</span>
-        <span className="hero-section__text--yellow">designer</span>
+        <span className="hero__text--yellow">designer</span>
       </h1>
     </section>
   );
