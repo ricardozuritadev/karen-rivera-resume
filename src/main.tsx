ec4ea2a007f199be148e89fcb8b866fdf819d2aa
Ricardo Zurita <ricardo.zurita.dev@gmail.com>
2024-03-01 09:55:20 +0100

    Import custom fonts

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
+import 'fonts/Lineal-Black.ttf';
+import 'fonts/Lineal-Thin.ttf';
+import 'fonts/Lineal-Regular.ttf';
 import 'styles/styles.scss';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
