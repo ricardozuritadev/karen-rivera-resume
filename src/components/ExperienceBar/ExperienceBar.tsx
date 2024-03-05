6e63bf2eaf97a7ed58201138c697254f8a576dde
Ricardo Zurita <ricardo.zurita.dev@gmail.com>
2024-03-05 09:33:44 +0100

    Implement classnames

--- src/components/ExperienceBar/ExperienceBar.tsx
+++ src/components/ExperienceBar/ExperienceBar.tsx
@@ -1,11 +1,22 @@
+import classNames from 'classnames';
+
 type ExperienceBarProps = {
   title: string;
   date: string;
+  index: number;
 };
 
-export default function ExperienceBar({ title, date }: ExperienceBarProps) {
+export default function ExperienceBar({
+  title,
+  date,
+  index
+}: ExperienceBarProps) {
+  const borderTopClass = classNames('experience-bar', {
+    'experience-bar--border-top': index === 0
+  });
+
   return (
-    <div className="experience-bar">
+    <div className={`experience-bar ${borderTopClass}`}>
       <p className="experience-bar__title">{title}</p>
       <p className="experience-bar__date">{date}</p>
     </div>
