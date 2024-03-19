c9a51132c2e20dd7f103e4acbe4136daa4526f7a
Ricardo Zurita <ricardo.zurita.dev@gmail.com>
2024-03-19 11:32:54 +0100

    Implement LazyLoadImage component

--- src/components/ProjectCard/ProjectCard.tsx
+++ src/components/ProjectCard/ProjectCard.tsx
@@ -1,3 +1,5 @@
+import { LazyLoadImage } from "react-lazy-load-image-component";
+
 type ProjectCardProps = {
   title: string;
   description: string;
@@ -14,17 +16,17 @@ export default function ProjectCard({
   return (
     <div className="project-card">
       <div className="project-card__info">
-        <img
+        <LazyLoadImage
           className="project-card__image"
-          src={image}
           alt="Project cover image"
+          src={image}
         />
         <div>
           <h3>{title}</h3>
           <h4>{description}</h4>
           {tags.map((tag, index) => (
             <span key={tag}>
-              {index > 0 ? ' |' : ''} {tag.toUpperCase()}
+              {index > 0 ? " |" : ""} {tag.toUpperCase()}
             </span>
           ))}
         </div>
