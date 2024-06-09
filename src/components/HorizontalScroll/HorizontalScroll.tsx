a7e3b60c7aeabcf5011a9a8d44eaa6b9ece74ba0
Ricardo Zurita <ricardo.zurita.dev@gmail.com>
2024-06-09 21:43:20 +0200

    Prettier format

--- src/components/HorizontalScroll/HorizontalScroll.tsx
+++ src/components/HorizontalScroll/HorizontalScroll.tsx
@@ -1,31 +1,34 @@
-import { useRef } from 'react';
-import { motion, useTransform, useScroll } from 'framer-motion';
+import { useRef } from "react";
+import { motion, useTransform, useScroll } from "framer-motion";
 
-import AnimatedText from 'components/AnimatedText';
+import AnimatedText from "components/AnimatedText";
 
 type HorizontalScrollProps = {
-  title: string;
-  children: React.ReactNode;
+    title: string;
+    children: React.ReactNode;
 };
 
 export default function HorizontalScroll({
-  title,
-  children
+    title,
+    children
 }: HorizontalScrollProps) {
-  const targetRef = useRef<HTMLDivElement | null>(null);
+    const targetRef = useRef<HTMLDivElement | null>(null);
 
-  const { scrollYProgress } = useScroll({ target: targetRef });
+    const { scrollYProgress } = useScroll({ target: targetRef });
 
-  const x = useTransform(scrollYProgress, [0.1, 0.95], ['0%', '-206%']);
+    const x = useTransform(scrollYProgress, [0.1, 0.95], ["0%", "-206%"]);
 
-  return (
-    <section ref={targetRef} className="horizontal-scroll">
-      <div className="horizontal-scroll__container">
-        <AnimatedText text={title} el="h2" />
-        <motion.div style={{ x }} className="horizontal-scroll__card-container">
-          {children}
-        </motion.div>
-      </div>
-    </section>
-  );
+    return (
+        <section ref={targetRef} className="horizontal-scroll">
+            <div className="horizontal-scroll__container">
+                <AnimatedText text={title} el="h2" />
+                <motion.div
+                    style={{ x }}
+                    className="horizontal-scroll__card-container"
+                >
+                    {children}
+                </motion.div>
+            </div>
+        </section>
+    );
 }
