ddff16ce2ec9737c505b6eab47e66f42b646a567
Ricardo Zurita <ricardo.zurita.dev@gmail.com>
2024-06-09 21:43:31 +0200

    Modify home constants

--- src/constants/home.constants.ts
+++ src/constants/home.constants.ts
@@ -1,15 +1,16 @@
 export const SECTIONS = {
-  HERO: {
-    HI: "Hi! I'm Karen",
-    SKILLS: {
-      PRODUCT: 'product',
-      UX_UI: 'ux ui',
-      WEB: 'web',
-      APP: 'app'
+    HERO: {
+        HI: "Hi! I'm Karen",
+        SKILLS: {
+            PRODUCT: "product",
+            UX_UI: "ux ui",
+            WEB: "web",
+            APP: "app"
+        },
+        DESIGNER: "designer"
     },
-    DESIGNER: 'designer'
-  },
-  PROJECTS: 'projects',
-  CHALLENGES: 'challenges',
-  MY_JOURNEY: 'my journey'
+    PROJECTS: "projects",
+    CHALLENGES: "challenges",
+    MY_JOURNEY: "my journey",
+    EXPERIENCE: "experience"
 };
