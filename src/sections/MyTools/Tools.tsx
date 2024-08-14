ee1cdffc296b155f51f0cf88253489c8644977f7
Ricardo Zurita <ricardoz@clickandgo.com>
2024-08-14 17:56:47 +0200

    Create tools section

--- src/sections/MyTools/Tools.tsx
+++ src/sections/MyTools/Tools.tsx
@@ -1,13 +1,94 @@
+import { Tooltip } from "react-tooltip";
+
 export default function Tools() {
     return (
-        <div className="tools">
-            <img src="icons/tools/figma.svg" alt="Figma logo" />
-            <img src="icons/tools/maze.svg" alt="Figma logo" />
-            <img src="icons/tools/lottie.svg" alt="Figma logo" />
-            <img src="icons/tools/ae.svg" alt="Figma logo" />
-            <img src="icons/tools/ps.svg" alt="Figma logo" />
-            <img src="icons/tools/ai.svg" alt="Figma logo" />
-            <img src="icons/tools/chatgpt.svg" alt="Figma logo" />
-        </div>
+        <>
+            <div className="tools">
+                <img
+                    src="icons/tools/figma.svg"
+                    alt="Figma logo"
+                    data-tooltip-id="figma-tooltip"
+                />
+                <img
+                    src="icons/tools/maze.svg"
+                    alt="Figma logo"
+                    data-tooltip-id="maze-tooltip"
+                />
+                <img
+                    src="icons/tools/lottie.svg"
+                    alt="Figma logo"
+                    data-tooltip-id="lottie-tooltip"
+                />
+                <img
+                    src="icons/tools/ae.svg"
+                    alt="Figma logo"
+                    data-tooltip-id="ae-tooltip"
+                />
+                <img
+                    src="icons/tools/ps.svg"
+                    alt="Figma logo"
+                    data-tooltip-id="ps-tooltip"
+                />
+                <img
+                    src="icons/tools/ai.svg"
+                    alt="Figma logo"
+                    data-tooltip-id="ai-tooltip"
+                />
+                <img
+                    src="icons/tools/chatgpt.svg"
+                    alt="Figma logo"
+                    data-tooltip-id="chatgpt-tooltip"
+                />
+            </div>
+
+            <Tooltip
+                id="figma-tooltip"
+                place="bottom"
+                content="Figma"
+                className="tools__tooltip"
+            />
+
+            <Tooltip
+                id="maze-tooltip"
+                place="bottom"
+                content="Maze"
+                className="tools__tooltip"
+            />
+
+            <Tooltip
+                id="lottie-tooltip"
+                place="bottom"
+                content="Lottiefiles"
+                className="tools__tooltip"
+            />
+
+            <Tooltip
+                id="ae-tooltip"
+                place="bottom"
+                content="After Effects"
+                className="tools__tooltip"
+            />
+
+            <Tooltip
+                id="ps-tooltip"
+                place="bottom"
+                content="Photoshop"
+                className="tools__tooltip"
+            />
+
+            <Tooltip
+                id="ai-tooltip"
+                place="bottom"
+                content="Adobe Illustrator"
+                className="tools__tooltip"
+            />
+
+            <Tooltip
+                id="chatgpt-tooltip"
+                place="bottom"
+                content="AI tools"
+                className="tools__tooltip"
+            />
+        </>
     );
 }
